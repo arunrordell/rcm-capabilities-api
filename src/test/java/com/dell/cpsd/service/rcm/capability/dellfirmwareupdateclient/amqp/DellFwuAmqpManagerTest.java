98a6e51deae24c02f9637854b7d989f43712fa9e
Rakesh <Rakesh.Kunjappa@emc.com>
2017-07-06 08:54:41 +0000

    Added few more unit tests and addressed review comments

--- src/test/java/com/dell/cpsd/service/rcm/capability/dellfirmwareupdateclient/amqp/DellFwuAmqpManagerTest.java
+++ src/test/java/com/dell/cpsd/service/rcm/capability/dellfirmwareupdateclient/amqp/DellFwuAmqpManagerTest.java
@@ -17,8 +17,12 @@ import com.dell.cpsd.service.rcm.capability.dellfirmwareupdateclient.amqp.consum
 import com.dell.cpsd.service.rcm.capability.dellfirmwareupdateclient.amqp.producer.IDellFwuAmqpProducer;
 
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import java.util.List;
+
+import static junit.framework.TestCase.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 /**
  * <p/>
@@ -74,6 +78,66 @@ public class DellFwuAmqpManagerTest
         }
     };
 
+    IDellFwuConfiguration producerNull = new IDellFwuConfiguration()
+    {
+        @Override
+        public IDellFwuAmqpConsumer getDellFwuConsumer()
+        {
+            IDellFwuAmqpConsumer iDellFwuAmqpConsumer = new IDellFwuAmqpConsumer()
+            {
+                @Override
+                public String getRoutingKey()
+                {
+                    return "dummyKey";
+                }
+
+                @Override
+                public IDellFwuAmqpMessageHandler getDellFwuMessageHandler()
+                {
+                    return null;
+                }
+
+                @Override
+                public void setDellFwuMessageHandler(final IDellFwuAmqpMessageHandler handler)
+                {
+
+                }
+            };
+            return iDellFwuAmqpConsumer;
+        }
+
+        @Override
+        public IDellFwuAmqpProducer getDellFwuProducer()
+        {
+            return null;
+        }
+    };
+
+    IDellFwuConfiguration consumerNull = new IDellFwuConfiguration()
+    {
+        @Override
+        public IDellFwuAmqpConsumer getDellFwuConsumer()
+        {
+            return null;
+        }
+
+        @Override
+        public IDellFwuAmqpProducer getDellFwuProducer()
+        {
+            IDellFwuAmqpProducer iDellFwuAmqpProducer = new IDellFwuAmqpProducer()
+            {
+                @Override
+                public void publishDellFwuComponent(final String timestamp, final String correlationId,
+                        final String rcmDellFwuRoutingKey, final String responseMessage, final List<CommandParameter> parameters)
+                        throws DellFwuServiceException
+                {
+
+                }
+            };
+            return iDellFwuAmqpProducer;
+        }
+    };
+
     private static final String randomUuid = "uuid";
     private static final long timeout = 10000L;
     private static final MessageProperties messageProperties = new MessageProperties(null,"correlationId",null);
@@ -109,6 +173,7 @@ public class DellFwuAmqpManagerTest
         DellFwuAmqpManager dellFwuAmqpManager = new DellFwuAmqpManager(iDellFwuConfiguration);
         dellFwuAmqpManager.handleDellFwuResponse(null);
         dellFwuAmqpManager.handleDellFwuResponse(updateFirmwareResponse);
+        assertNotNull(dellFwuAmqpManager.getRcmDellFwuProducer());
     }
 
     @Test
@@ -119,5 +184,43 @@ public class DellFwuAmqpManagerTest
         DellFwuAmqpManager dellFwuAmqpManager = new DellFwuAmqpManager(iDellFwuConfiguration);
         dellFwuAmqpManager.handleDellFwuError(null);
         dellFwuAmqpManager.handleDellFwuError(updateFirmwareErrorMessage);
+        assertNotNull(dellFwuAmqpManager.getRcmDellFwuConsumer());
     }
+
+    @Test
+    public void testRoutingKey() throws Exception
+    {
+        String routingKey = "routingKey";
+        DellFwuAmqpManager dellFwuAmqpManager = new DellFwuAmqpManager(iDellFwuConfiguration);
+        dellFwuAmqpManager.setRoutingKey(routingKey);
+        assertEquals(dellFwuAmqpManager.getRoutingKey(),routingKey);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testRoutingKeyNull() throws Exception
+    {
+        DellFwuAmqpManager dellFwuAmqpManager = new DellFwuAmqpManager(iDellFwuConfiguration);
+        dellFwuAmqpManager.setRoutingKey(null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testSetRcmDellFwuConsumerNull()
+    {
+       DellFwuAmqpManager dellFwuAmqpManager = new DellFwuAmqpManager(consumerNull);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testSetRcmDellFwuProducerNull()
+    {
+       DellFwuAmqpManager dellFwuAmqpManager = new DellFwuAmqpManager(producerNull);
+    }
+
+    @Test(expected = DellFwuServiceException.class)
+    public void testGetDellFwuShutDownTrue() throws Exception
+    {
+        DellFwuAmqpManager dellFwuAmqpManager = Mockito.spy(new DellFwuAmqpManager(iDellFwuConfiguration));
+        Mockito.when(dellFwuAmqpManager.isShutDown()).thenReturn(true);
+        dellFwuAmqpManager.getDellFwu(randomUuid, timeout);
+    }
+
 }
